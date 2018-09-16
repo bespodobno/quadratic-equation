c25f954030db746bbd0fdd67cfa4855343d15af2
bespodobno <43144813+bespodobno@users.noreply.github.com>
2018-09-17 01:48:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let reg = /(-?\s?\d+)/g
+    let numbersWithSign = equation.match(reg);
+    let numbersWithoutSpace = numbersWithSign.map(number=>number.replace(" ",""));
+    let finalNumbers = numbersWithoutSpace.filter((str,index)=>index!==1);
+
+    var discriminant = finalNumbers[1]*finalNumbers[1] - 4*finalNumbers[0]*finalNumbers[2];
+
+    var arr = [];
+    arr.push(Math.round((-finalNumbers[1] + Math.sqrt(discriminant))/(2*finalNumbers[0])));
+    arr.push((Math.round(-finalNumbers[1] - Math.sqrt(discriminant))/(2*finalNumbers[0])));
+
+return arr.sort(compareNumeric);
+}
+
+function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
 }
